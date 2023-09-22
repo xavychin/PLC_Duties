d3a5ae7ff81000065d5b278f4e3682c1ee4f61fd
Xavier Chin <xavychin@gmail.com>
2023-09-22 22:25:51 +0800

    test

--- Duties_Functions.js
+++ Duties_Functions.js
@@ -1,6 +1,19 @@
 function curr_date(){
-    var date = new Date();
+    var tmr = new Date();
+    tmr.setDate(tmr.getDate()+1);
 
-    date = date.substring(4,15);
-    document.title = "Duty for" + date;
+    let tmr_string = tmr.toString();
+    tmr_string = tmr_string.slice(4,15);
+    document.title = "Duty for " + tmr_string;
+
+    document.getElementById('date_time').textContent = "Duty for " + tmr_string;
+
+}
+
+function ttl_duty_personnel(){
+    var cbts = document.getElementById("cbts").textContent
+    var svc = document.getElementById("svc").textContent
+    var med_appt = document.getElementById("med_appt").textContent
+    var med_leave = document.getElementById("med_leave").textContent
+    var leave_off = document.getElementById("leave_off").textContent
 }
