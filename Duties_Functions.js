9f435f47716439d6473f3c714d997b3558374a99
Xavier Chin <xavychin@gmail.com>
2023-09-23 22:09:45 +0800

    test

--- Duties_Functions.js
+++ Duties_Functions.js
@@ -11,9 +11,45 @@ function curr_date(){
 }
 
 function ttl_duty_personnel(){
-    var cbts = document.getElementById("cbts").textContent
-    var svc = document.getElementById("svc").textContent
-    var med_appt = document.getElementById("med_appt").textContent
-    var med_leave = document.getElementById("med_leave").textContent
-    var leave_off = document.getElementById("leave_off").textContent
+    var cbts = document.getElementById("cbts").value
+    var svc = document.getElementById("svc").value
+    var med_appt = document.getElementById("med_appt").value
+    var med_leave = document.getElementById("med_leave").value
+    var leave_off = document.getElementById("leave_off").value
+
+    const types = ["cbts", "svc", "med_appt", "med_leave", "leave_off"];
+
+    var ttl_ppl; 
+
+    for(let j = 0; j < types.length; j++){
+        var cbt_array = cbts.split(/\n/);
+        ttl_ppl = ttl_ppl + cbt_array.length;
+
+        var duties_class = document.querySelectorAll(".duties");
+
+        if(duties_class.length>0){
+            if(duties_class[0].innerText==""){
+
+            }
+            else{
+                for(let x = 0; x < duties_class.length; x++){
+                    document.getElementById(duties_class[x].innerText).remove();
+                }
+            }
+        }
+
+        for (let i = 0; i < cbt_array.length; i++) {
+            if(cbts!=""){
+                const row = document.createElement("tr");
+                row.setAttribute("id", cbt_array[i]);
+                row.setAttribute("class", "duties"); 
+                row.textContent = cbt_array[i];
+
+                duty_tbl = document.getElementById("duty_tbl");
+                duty_tbl.appendChild(row);
+            }
+            
+        }
+    }
+
 }
